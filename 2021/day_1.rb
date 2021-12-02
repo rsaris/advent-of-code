07a0f9a60f9b9987436543e56f439b640f676500
Bob Saris <bob@bobsaris.com>
2021-12-01 22:52:06 -0500

    2021 Day 1 Part 2

--- 2021/day_1.rb
+++ 2021/day_1.rb
@@ -2,10 +2,24 @@ def read_depths(file_name)
   File.open("./inputs/#{file_name}").read.split("\n").map(&:to_i)
 end
 
+def build_depth_groups(depths, size = 3)
+  depths[..-size].each_with_index.map { |d, i| depths[i] + depths[i+1] + depths[i+2] }
+end
+
+def count_increases(depths)
+  depths[1..].each_with_index.count { |d, i| d > depths[i] }
+end
+
 def do_part_1
-  depths = read_depths('day_1.txt')
-  puts depths[1..].each_with_index.count { |d, i| d > depths[i] }
+  puts count_increases(read_depths('day_1.txt'))
+end
+
+def do_part_2
+  puts count_increases(build_depth_groups(read_depths('day_1.txt')))
 end
 
 puts 'PART 1'
 do_part_1 # 1316
+
+puts 'PART 2'
+do_part_2 # 1344
