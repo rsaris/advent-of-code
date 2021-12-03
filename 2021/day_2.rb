b04deb59dfe96102d171eecd4495cbd1660e9d56
Bob Saris <bob@bobsaris.com>
2021-12-02 21:47:13 -0500

    2021 Day 2 Part 2

--- 2021/day_2.rb
+++ 2021/day_2.rb
@@ -8,5 +8,15 @@ def do_part_1
   puts submarine.x * submarine.y
 end
 
+def do_part_2
+  instructions = read_file('day_2.txt')
+  submarine = Submarine.new
+  submarine.follow_aim_instructions(instructions)
+  puts submarine.x * submarine.y
+end
+
 puts 'PART 1'
 do_part_1 # 1962940
+
+puts 'PART 2'
+do_part_2
--- 2021/lib/submarine.rb
+++ 2021/lib/submarine.rb
@@ -3,15 +3,34 @@ class Submarine
   DIR_DOWN = 'down'
   DIR_UP = 'up'
 
-  attr_reader :x, :y
+  attr_reader :aim, :x, :y
 
   def initialize
     reset_position
   end
 
   def reset_position
+    @aim = 0
     @x = 0
     @y = 0
+  end
+
+  def follow_aim_instructions(instructions)
+    instructions.each do |instruction|
+      dir, amount = instruction.split(' ')
+      amount = amount.to_i
+      case dir
+      when DIR_FORWARD
+        @x += amount
+        @y += @aim * amount
+      when DIR_UP
+        @aim -= amount
+      when DIR_DOWN
+        @aim += amount
+      else
+        raise "Unknown direction #{dir} (AMOUNT = #{amount})"
+      end
+    end
   end
 
   def follow_simple_instructions(instructions)
