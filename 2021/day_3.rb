f1170f97e0ea10f599388203e775e9583ae22d04
Bob Saris <bob@bobsaris.com>
2021-12-05 14:09:13 -0500

    2021 Day 3 Part 2.

--- 2021/day_3.rb
+++ 2021/day_3.rb
@@ -7,5 +7,14 @@ def do_part_1
   puts gamma.to_i(2) * epsilon.to_i(2)
 end
 
+def do_part_2
+  inputs = read_file('day_3.txt')
+  oxygen, carbon_dioxide = Diagnostics.new(inputs).calculate_ratings
+  puts oxygen.to_i(2) * carbon_dioxide.to_i(2)
+end
+
 puts 'PART 1'
 do_part_1 # 3429254
+
+puts 'PART 2'
+do_part_2 # 5410338
--- 2021/lib/diagnostics.rb
+++ 2021/lib/diagnostics.rb
@@ -1,15 +1,51 @@
 class Diagnostics
-  attr_reader :diagnostic_report
+  attr_reader :debug, :diagnostic_report
 
-  def initialize(diagnostic_report)
+  def initialize(diagnostic_report, debug: false)
+    @debug = debug
     @diagnostic_report = diagnostic_report
   end
 
   def calculate_rates
-    num_inputs = diagnostic_report.size
+    calculate_rates_from_inputs(diagnostic_report)
+  end
+
+  def calculate_ratings
+    oxygen_inputs = diagnostic_report.dup
+    carbon_dioxide_inputs = diagnostic_report.dup
+
+    cur_pos = 0
+    while oxygen_inputs.size > 1 || carbon_dioxide_inputs.size > 1
+      cur_pos = 0 if cur_pos > (oxygen_inputs[0].size - 1)
+      puts "[DEBUG] Looking at #{cur_pos}" if debug
+
+      if oxygen_inputs.size > 1
+        gamma, _ = calculate_rates_from_inputs(oxygen_inputs)
+        puts "[DEBUG] Found gamma #{gamma}" if debug
+        oxygen_inputs = oxygen_inputs.select { |input| input[cur_pos] == gamma[cur_pos] }
+        puts "[DEBUG] Filtered to #{oxygen_inputs.join(', ')}" if debug
+      end
+
+      if carbon_dioxide_inputs.size > 1
+        _, epsilon = calculate_rates_from_inputs(carbon_dioxide_inputs)
+        puts "[DEBUG] Found espsilon #{epsilon}" if debug
+        carbon_dioxide_inputs = carbon_dioxide_inputs.select { |input| input[cur_pos] == epsilon[cur_pos] }
+        puts "[DEBUG] Filtered to #{carbon_dioxide_inputs.join(', ')}" if debug
+      end
+
+      cur_pos += 1
+    end
+
+    [oxygen_inputs.first, carbon_dioxide_inputs.first]
+  end
+
+  private
+
+  def calculate_rates_from_inputs(inputs)
+    num_inputs = inputs.size
 
     bit_counts = []
-    diagnostic_report.each do |input|
+    inputs.each do |input|
       input.split('').each_with_index do |bit, pos|
         bit_counts[pos] ||= 0
         if bit == '1'
