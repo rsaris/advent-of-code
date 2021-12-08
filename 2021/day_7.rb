a6eec4640e7d0e4d48e179d9d9fd0926b4ceed93
Bob Saris <bob@bobsaris.com>
2021-12-07 21:59:57 -0500

    Day 7 Part 2

--- 2021/day_7.rb
+++ 2021/day_7.rb
@@ -23,5 +23,31 @@ def do_part_1
   puts fuel_costs.min_by { |k, v| v }.last
 end
 
+def do_part_2
+  debug = false
+
+  inputs = read_file('day_7.txt')
+  initial_position_map = inputs[0].split(',').map(&:to_i).reduce(Hash.new(0)) { |acc, pos| acc[pos] += 1; acc }
+  puts "Found unique positions #{initial_position_map.keys.size}" if debug
+
+  min_position = initial_position_map.keys.min
+  max_position = initial_position_map.keys.max
+  puts "Crabs are between #{min_position} and #{max_position}" if debug
+
+  fuel_costs = {}
+  (min_position..max_position).each do |position|
+    puts "Testing position #{position}" if debug
+    fuel_costs[position] =
+      initial_position_map.map do |pos, num|
+        (1..(pos - position).abs).sum * num
+      end.sum
+  end
+
+  puts fuel_costs.min_by { |k, v| v }.last
+end
+
 puts 'PART 1'
 do_part_1 # 337488
+
+puts 'PART 2'
+do_part_2 # 89647695
