148bee1f4dc67144da45ca1b55275eb5f32c8eab
Bob Saris <bob@bobsaris.com>
2021-12-07 21:35:27 -0500

    Day 6 Part 2

--- 2021/day_6.rb
+++ 2021/day_6.rb
@@ -1,6 +1,6 @@
 require_relative 'lib/read_file'
 
-def do_part_1
+def run_sim(num_days)
   debug = false
 
   inputs = read_file('day_6.txt')[0].split(',').map(&:to_i)
@@ -14,7 +14,7 @@ def do_part_1
   days_out_2 = 0
   day_out_1 = 0
 
-  80.times do |i|
+  num_days.times do |i|
     spawn_day = i % 7
     puts "DAY #{i}: Running day #{spawn_day} -- found #{spawn_days[spawn_day]} fish for today" if debug
 
@@ -31,5 +31,16 @@ def do_part_1
   puts spawn_days.sum + day_out_1 + days_out_2
 end
 
+def do_part_1
+  run_sim(80)
+end
+
+def do_part_2
+  run_sim(256)
+end
+
 puts 'PART 1'
 do_part_1 # 359999
+
+puts 'PART 2'
+do_part_2 # 1631647919273
