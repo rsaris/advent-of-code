5cac0c84976057ae7b33396021c64683c1a59c2d
Bob Saris <bob@userinterviews.com>
2022-12-09 14:51:58 -0500

    Day 9 part 2

--- 2022/day_9.rb
+++ 2022/day_9.rb
@@ -6,6 +6,47 @@ COMMAND_REGEX = /^(?<direction>[UDLR]) (?<steps>\d*)/
 
 Point = Struct.new(:x, :y)
 
+def update_point_for_command(point, command)
+  command_match = command.match(COMMAND_REGEX)
+  case command_match['direction']
+  when 'U'
+    point.y = point.y + command_match['steps'].to_i
+  when 'D'
+    point.y = point.y - command_match['steps'].to_i
+  when 'R'
+    point.x = point.x + command_match['steps'].to_i
+  when 'L'
+    point.x = point.x - command_match['steps'].to_i
+  end
+
+  point
+end
+
+def reconcile_point(head_point, next_point, tracking_hash = nil)
+  x_delta = head_point.x - next_point.x
+  y_delta = head_point.y - next_point.y
+
+  if (x_delta.abs > 1 && y_delta.abs > 0) || (x_delta.abs > 0 && y_delta.abs > 1)
+    next_point.x += x_delta / x_delta.abs
+    next_point.y += y_delta / y_delta.abs
+  elsif x_delta.abs > 1
+    next_point.x += x_delta / x_delta.abs
+  elsif y_delta.abs > 1
+    next_point.y += y_delta / y_delta.abs
+  end
+
+  if tracking_hash
+    tracking_hash[next_point.x] ||= {}
+    tracking_hash[next_point.x][next_point.y] = true
+  end
+end
+
+def reconcile_points(head_point, next_point, tracking_hash = nil)
+  while (head_point.x - next_point.x).abs > 1 || (head_point.y - next_point.y).abs > 1
+    reconcile_point(head_point, next_point, tracking_hash)
+  end
+end
+
 def do_part_1
   head_point = Point.new(0, 0)
   tail_point = Point.new(0, 0)
@@ -14,38 +55,26 @@ def do_part_1
 
   lines = read_file('day_9.txt')
   lines.each do |line|
-    line_match = line.match(COMMAND_REGEX)
-    case line_match['direction']
-    when 'U'
-      head_point.y = head_point.y + line_match['steps'].to_i
-    when 'D'
-      head_point.y = head_point.y - line_match['steps'].to_i
-    when 'R'
-      head_point.x = head_point.x + line_match['steps'].to_i
-    when 'L'
-      head_point.x = head_point.x - line_match['steps'].to_i
-    end
+    update_point_for_command(head_point, line)
+    reconcile_points(head_point, tail_point, tail_positions)
+  end
 
-    x_delta = head_point.x - tail_point.x
-    y_delta = head_point.y - tail_point.y
+  puts tail_positions.values.sum { |array| array.count }
+end
 
-    while x_delta.abs > 1 || y_delta.abs > 1
-      if (x_delta.abs > 1 && y_delta.abs > 0) || (x_delta.abs > 0 && y_delta.abs > 1)
-        tail_point.x += x_delta / x_delta.abs
-        tail_point.y += y_delta / y_delta.abs
-      elsif x_delta.abs > 1
-        tail_point.x += x_delta / x_delta.abs
-      elsif y_delta.abs > 1
-        tail_point.y += y_delta / y_delta.abs
-      else
-        binding.pry
-        raise "Unexpected deltas found #{x_delta} #{y_delta} on line #{line}"
+def do_part_2
+  points = (0..9).map { |i| Point.new(0, 0) }
+
+  tail_positions = { 0 => { 0 => true } } # Set 0,0 to true
+
+  lines = read_file('day_9.txt')
+  lines.each do |line|
+    update_point_for_command(points[0], line)
+    while (points[0].x - points[1].x).abs > 1 || (points[0].y - points[1].y).abs > 1
+      (1..8).each do |index|
+        reconcile_point(points[index-1], points[index])
       end
-
-      tail_positions[tail_point.x] ||= {}
-      tail_positions[tail_point.x][tail_point.y] = true
-      x_delta = head_point.x - tail_point.x
-      y_delta = head_point.y - tail_point.y
+      reconcile_point(points[8], points[9], tail_positions)
     end
   end
 
@@ -54,3 +83,6 @@ end
 
 puts 'PART 1'
 do_part_1 # 6354
+
+puts 'PART 2'
+do_part_2 # 2651
