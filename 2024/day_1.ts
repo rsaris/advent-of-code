1f1e83c6a8613c39dc3eeab631c8a43e0e3ab6ec
Bob Saris <bob@userinterviews.com>
2024-12-01 23:12:04 -0500

    Setup and day 1

---
+++ 2024/day_1.ts
@@ -0,0 +1,51 @@
+const { readFileSync } = require('node:fs');
+
+async function parseInput() {
+  const content = readFileSync(
+    './inputs/day_1.txt',
+    { encoding: 'utf8', flag: 'r' },
+  );
+  const leftArray = [] as number[];
+  const rightArray = [] as number[];
+
+  content.split('\n').forEach((row: string) => {
+    if (row !== '') {
+      const values = row.split('   ');
+      leftArray.push(parseInt(values[0]));
+      rightArray.push(parseInt(values[1]));
+    }
+  });
+
+  return [leftArray.sort(), rightArray.sort()];
+}
+
+async function part1() {
+  const [leftArray, rightArray] = await parseInput();
+
+  let count = 0;
+  for (let i = 0; i < leftArray.length; i++) {
+    count += Math.abs(leftArray[i] - rightArray[i]);
+  }
+
+  console.log(count);
+}
+
+async function part2() {
+  const [leftArray, rightArray] = await parseInput();
+
+  let score = 0;
+  leftArray.forEach(leftNum => {
+    let numInRight = 0;
+    rightArray.forEach(rightNum => {
+      if (leftNum === rightNum) {
+        numInRight++;
+      }
+    })
+    score += leftNum * numInRight;
+  });
+
+  console.log(score);
+}
+
+part1(); // 1666427
+part2(); // 24316233
